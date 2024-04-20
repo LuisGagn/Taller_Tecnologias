4b7df636c0a58615a33585248583b5cd863b15d1
Luis G <luisgagnevin@gmail.com>
2024-04-20 19:47:20 -0300

    Finalizacion puntos consulta
    Completado.

--- LetraIni.sh
+++ LetraIni.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-
+# Valores Iniciales
 letraInicial=0
 letraFinal=0
 letraContenida=0
-usuario=Luis
+usuario=Luis #Motivos de test.
 	
 function tildes {
 	# Declara un array asociativo ([a]=aá) (Por eso -A)
+	# Al usar [xy] el programa lo evalua como un x OR y 
 	declare -A letras
-	letras=([a]=aá [e]=eé [i]=ií [o]=oó [u]=uú)
-	vocales=aeiouAEIOU
+	letras=([a]=[aá] [e]=[eé] [i]=[ií] [o]=[oó] [u]=[uú])
+	vocales=aeiou
 	if [[ $vocales =~ $1 ]];
 	then 
 		echo ${letras[$1]} 
 	else 
-		echo $1$1
+		echo "[$1$1]"
 	fi
 }
 	
@@ -23,7 +24,8 @@ function letra_inicial {
 	echo "Ingrese la letra inicial"
 	echo "Si desea salir presione 0"
 	read letra 
-	
+	# Verifica si se ingresa 1 o mas letras. 
+	# En caso de ingresar +1 realiza la recursion correspondiente.
 	if [ $letra != 0 ]; then 
 		if [ ${#letra} -le 1 ]; then
 			letraInicial=${letra,,}
@@ -40,15 +42,16 @@ function letra_contenida {
 	echo "Ingrese la letra contenida"
 	echo "Si desea salir presione 0"
 	read letra 
-	
+	# Verifica si se ingresa 1 o mas letras. 
+	# En caso de ingresar +1 realiza la recursion correspondiente.
 	if [ $letra != 0 ]; then 
 		if [ ${#letra} -le 1 ]; then
 			letraContenida=${letra,,}
-			echo "La letra Final sera $letraContenida"
+			echo "La letra contenida sera $letraContenida"
 			letraContenida=$(tildes $letra) 
 		else 
 			echo "Ingrese una sola letra"
-			letra_inicial
+			letra_contenida
 		fi
 	fi
 }
@@ -57,102 +60,148 @@ function letra_final {
 	echo "Ingrese la letra final"
 	echo "Si desea salir presione 0"
 	read letra 
-	
+	# Verifica si se ingresa 1 o mas letras. 
+	# En caso de ingresar +1 realiza la recursion correspondiente.
 	if [ $letra != 0 ]; then 
 		if [ ${#letra} -le 1 ]; then
 			letraFinal=${letra,,}
-			echo "La letra inicial sera $letraFinal"
+			echo "La letra final sera $letraFinal"
 			letraFinal=$(tildes $letra) 
 		else 
 			echo "Ingrese una sola letra"
-			letra_inicial
+			letra_final
 		fi
 	fi
 }
 
+function rwrite () {
+	echo "Desea crear un nuevo archivo o sobreescribir el original"
+	echo "Archivo por defecto: resultados.txt"
+	echo "1 - Crear archivo"
+	echo "2 - Sobreescribir original"
+	echo "Escriba 1 o 2 segun corresponda"
+
+	read eleccion
+
+	if [ $eleccion == 1 ]; 
+	then 
+	echo "Ingrese nombre de archivo sin extension"
+	read archivo
+	output="$archivo.txt"
+	else
+	output=resultados.txt
+	fi
+}
+
 function consultar_diccionario () {
-	# Como para tomar los tildes guardamos aá 
-	# y no hay un string aá en las palabras
-	# como queremos ver si tiene a o á 
-	# la dividimos en 2 letras.
-	letraContenidaT=${letraContenida:1:1}
-	letraContenidaST=${letraContenida:0:1}
+
 	
-	## Chequear letras != 0
-	
+
+	## Valida que se hayan ingresado las 3 letras.
+	if [ $letraInicial == 0 ] || 
+	[ $letraFinal == 0 ] ||
+	[ $letraContenida == 0 ]; 
+	then
+	echo "Verifique que todas las letras hayan sido escogidas.
+		 Letra Inicial: ${letraInicial:1:1}
+		 Letra Contenida: ${letraContenida:1:1}
+		 Letra Final: ${letraFinal:1:1}"
+	return
+	fi
+
+	rwrite
 	##Contadores
 	cantidadPalabras=0
 	palabrasTotales=0
 	
-	## Write an remove
-	output=resultados.txt
+		
+	# Archivo auxiliar | Ver si puede cambiarse por una variable.
+	auxiliar=auxiliar.txt
+	if [ -e "$auxiliar" ];
+	then
+		rm "$auxiliar"	
+	fi
+
 	
 	if [ -e "$output" ];
 	then
+		echo Sobreescribiendo $output
 		rm "$output"
-		echo "Generando nuevo archivo"
 	else
 		echo "Generando archivo $output"
 	fi
 	
-	echo "" >> $output
-	echo "Lista de palabras encontradas" >> $output
-	echo "_____________________________" >> $output
-	echo "" >> $output
-	
+	start=$(date +%s.%N) #IGNORAR benchmarking
 
 	## Lee linea a linea
 	while IFS= read line; 
 		do
-			# Al existir palabras como "autentica verbo"
-			# en el diccionario es necesario borrar "verbo" 
-			# ya que nos dara resultados erroneos.
-			if [[ $line =~ ' ' ]]; 
-			then
-				line=$(echo $line | cut -d' ' -f1)
-			fi
-			
 			# Si hay palabras de 1 o 2 caracteres
-			# o reonglones en blanco los ignora.
-			if [ ${#line} -ge 3 ]; 
-			then
+			# o strings en blanco los ignora.
+			## $(()) permite realizar operaciones 
+			# ${#a} devuelve largo de a 
+			largo=$((${#line})) 				
 
-			## $(()) deja operar $(#a) largo de a 
-			largo=$((${#line})) 
+			if [ $largo -ge 3 ]; 
+			then
+				# Al existir palabras como "autentica verbo"
+				# en el diccionario es necesario borrar "verbo" 
+				# ya que nos dara resultados erroneos.
+				if [[ $line =~ ' ' ]]; 
+				then
+					line=$(echo $line | cut -d' ' -f1)
+				fi
 			
 			# subStrings -> Palabra
 				inicial=${line:0:1}
-				final=${line:largo-1:largo}	
 				contenida=${line:1:-1} 
+				final=${line:0-1}	# Si usamos "-1" solo o :0:-1 devuelve toda la linea. es importante usar " -1" o "0-1" 
 
 			# Realiza varios checks
 			# la funcion [[ $a =~ $b ]] 
 			# revisa si en $a existe el string $b
 			if [[ $letraInicial =~ $inicial ]] && 
 			[[ $letraFinal =~ $final ]] && 
-			([[ $contenida =~ $letraContenidaST ]] || 
-			[[ $contenida =~ $letraContenidaT ]]) ; 
+			[[ $contenida =~ $letraContenida ]] 
 			then
 				#Escribe palabra y suma
 				cantidadPalabras=$((cantidadPalabras+1))
-				echo $line >> $output
+				echo $line >> $auxiliar
 				fi
 			fi
 		
 			palabrasTotales=$((palabrasTotales+1))
 		done < diccionario.txt
 	
-	
-	
 	# Header del documento
 	porcentaje=$(bc <<< "scale=2;($cantidadPalabras*100)/$palabrasTotales")
-	sed -i '1s/^/'"0$porcentaje% de las palabras coinciden."'\n/' $output
-	sed -i '1s/^/'"Total de palabras analizadas: $palabrasTotales"'\n/' $output
-	sed -i '1s/^/'"$(date +%F)"'\n/' $output
-	sed -i '1s/^/'"Usuario: $usuario"'\n/' $output
+	header="Usuario : $usuario\n$(date +%F)\nTotal de palabras analizadas: $palabrasTotales\nTotal de palabras validas: $cantidadPalabras\n0$porcentaje% de las palabras coinciden.\n"
 	
+	if [ $cantidadPalabras != 0 ]; 
+	then 
+#	sed -i "1i\\$header" $auxiliar
+	# Por algun motivo algunas palabras al realizar echo $line >> $auxiliar las ingresa en el mismo renglon
+	# Esta funcion awk busca rapidamente todos los campos que tengan exactamente 2 palabras (Maximo ocurrente) 
+	# y reingresa en ese orden dentro del documento a la segunda palabra en una fila nueva
+	awk 'NF==2 {print $1"\n"$2} NF!=2' $auxiliar > $output
+	rm $auxiliar
 	
-	echo "Archivo guardado con nombre $output"
+	else 
+	echo "" >> $output	
+
+	echo "No existen palabras para la combinacion:		
+		Letra Inicial: ${letraInicial:1:1}
+		Letra Contenida: ${letraContenida:1:1}
+		Letra Final: ${letraFinal:1:1}
+		Pruebe con otra combinacion"
+	fi
+	sed -i "1i\\$header" $output
+
+# runtime IGNORAR benchmarking.
+end=$(date +%s.%N)
+runtime=$(echo "$end - $start" | bc)
+
+echo "Finalizado en $runtime segundos"
 	
 }
 
